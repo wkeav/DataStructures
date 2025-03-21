45965479089a4c4605ce490fb9412a0e7b358315
Astra <astra.k.nguyen05@outlook.com>
2025-03-20 19:38:27 -0500

    update codes

--- Node/Node.java
+++ Node/Node.java
@@ -1,27 +1,45 @@
 /*
- * 
+ * Open an ice cream shop with three values : strawberry, banana and coconut.
+ * The nodes will connect different orders together - sundae, single cup, etc
  */
 public class Node {
     public String data;   
     private Node next; 
 
+    //constructor
     public Node(String data){
     this.data = data;
     this.next = null;
     }
 
-    private void setNextNode(Node node){
+    public void setNextNode(Node node){
         this.next = node;
     }
 
-    private Node getNextNode(){
+    public Node getNextNode(){
         return this.next;
     }
 public static void main(String[] args) {
-    Node firstNode = new Node("I am a Node!");
-    Node secondNode = new Node("I am the second node!");
+    // Node firstNode = new Node("I am a Node!");
+    // Node secondNode = new Node("I am the second node!");
 	// System.out.println(firstNode.data);
-    System.out.println(firstNode.next.data);
+    // System.out.println(firstNode.getNextNode().data);
 
+    //sundae 
+    Node strawberry = new Node("Berry Tasty");
+    Node banana = new Node("Banana-rama");
+    Node coconut = new Node("Nuts for Coconut");
+
+    strawberry.setNextNode(banana);
+    banana.setNextNode(coconut);
+
+    Node currentNode = strawberry;
+    while (currentNode != null) {
+        System.out.println(currentNode.data);
+        currentNode = currentNode.getNextNode();
+    }
+
+    //one cup
+    System.out.println("\n" + strawberry.data);
 	}
 }
